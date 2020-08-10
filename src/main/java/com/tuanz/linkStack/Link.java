6bc8f3c22939463a2c22b92303a248e2dbc346b1
吴敬俊 <635813714@qq.com>
2020-08-10 09:31:17 +0800

    用链表实现的栈

---
+++ src/main/java/com/tuanz/linkStack/Link.java
@@ -0,0 +1,16 @@
+package com.tuanz.linkStack;
+
+public class Link {
+
+    public long dData;
+    public Link next;
+
+    public Link(long d){
+        dData = d;
+    }
+
+    public void displayLink(){
+        System.out.print(dData + " ");
+    }
+
+}
