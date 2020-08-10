6c4d85c517a17ccca8d6dfec5a192c8c4c6fc8e7
吴敬俊 <635813714@qq.com>
2020-08-10 18:05:54 +0800

    递归和 高级排序

---
+++ src/main/java/com/tuanz/sort01/PartitionApp.java
@@ -0,0 +1,30 @@
+package com.tuanz.sort01;
+
+import com.tuanz.array.Array;
+
+/**
+ * 划分
+ */
+public class PartitionApp {
+
+    public static void main(String[] args) {
+        int maxSize = 16;
+        ArrayPar arr;
+        arr = new ArrayPar(maxSize);
+
+        for (int j=0; j<maxSize; j++){
+            long n = (int)(java.lang.Math.random()*199);
+            arr.insert(n);
+        }
+        arr.display();
+
+        long pivot = 99;
+        System.out.print("Pivot is "+ pivot);
+        int size = arr.size();
+
+        int partDex = arr.partitionIt(0,size-1,pivot);
+
+        System.out.println(", Partition is at index "+ partDex);
+        arr.display();
+    }
+}
